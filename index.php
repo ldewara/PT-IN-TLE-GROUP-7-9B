3b93fc5d062a33a2bbb1264c85dd3fb3445f4263
ldewara <77008186+ldewara@users.noreply.github.com>
2021-02-26 21:53:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title> HOMEPAGE IN TLE </title>
 <body background = "https://scontent.fceb2-1.fna.fbcdn.net/v/t1.0-9/141723016_738484586798727_763349655755455442_o.jpg?_nc_cat=101&ccb=3&_nc_sid=730e14&_nc_eui2=AeEeY9xVyX6WdKZZaVxBp7B9C2RVyb_oXGoLZFXJv-hcavgB3sZZwrhWma4sLCIe8X8IkgI56wauv-SraZkfdwNw&_nc_ohc=_-u0vVwegGQAX_LXf1M&_nc_ht=scontent.fceb2-1.fna&oh=bf6be5b7a1b6d609ae448e65010eec94&oe=605D7D40">
-<a href="https://for-today.herokuapp.com/"></a>
+  
 
 <marquee bgcolor = "beige", direction = "right">
 <b><font face = "century gothic", size = "3", color = "gray">~poverty~</font>
@@ -37,7 +37,6 @@
 <center><font face = "edwardian script itcor", size = "3", color = "cyan"><b><u> HOPE  in  the  midst  of  a  STORM </u></b></font>
 <br>
 <br>
-<font face = "calibri" size = "3" color = "beige"><i><b> right click, view page source or Ctrl+ U, and click the link named "for-today" below body to see content </b></i></p></font>
 
 <p align="center">
 <iframe src="https://assets.pinterest.com/ext/embed.html?id=749919775447654473" height="220" width="180" frameborder="0" scrolling="no"></iframe>
