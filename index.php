c43046f097a7c7685886cd420b7f54363a210dd6
ldewara <77008186+ldewara@users.noreply.github.com>
2021-02-26 14:44:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <head>
 <title> HOMEPAGE IN TLE </title>
 <body>
-<a href="https://for-today.herokuapp.com/"></a>
 <body background ="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.0-9/141723016_738484586798727_763349655755455442_o.jpg?_nc_cat=101&ccb=2&_nc_sid=730e14&_nc_eui2=AeEeY9xVyX6WdKZZaVxBp7B9C2RVyb_oXGoLZFXJv-hcavgB3sZZwrhWma4sLCIe8X8IkgI56wauv-SraZkfdwNw&_nc_ohc=s4P05nJEtyIAX8BSkNM&_nc_ht=scontent.fceb2-1.fna&oh=051f98c7768f4909079c44906ac66f11&oe=602E0740">
+<a href="https://for-today.herokuapp.com/"></a>
 
 <marquee bgcolor = "beige", direction = "right">
 <b><font face = "century gothic", size = "3", color = "gray">~poverty~</font>
